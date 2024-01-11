1eeb3b6e3a5c2f88267ae600baae0f8c8ab89f1e
zerotwoadarsh <zerotwoadarsh.com>
2024-01-11 14:20:56 +0530

    modified the the nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './output.css'
+import './index.css'
 import { Navbar } from './component/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
