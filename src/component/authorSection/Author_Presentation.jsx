35ae2ec993eb2895d91e4f24fba2060d9f8d404d
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-02-24 12:55:37 +0530

    submission css

--- src/component/authorSection/Author_Presentation.jsx
+++ src/component/authorSection/Author_Presentation.jsx
@@ -3,7 +3,7 @@ import presentationBanner from '../../assets/images/registrsationBanner.jpg'
 
 const Author_Presentation = () => {
     return (
-        <div className=' bg-white text-black pb-5 mr-5 ml-5'>
+        <div className=' bg-white text-black pb-5 '>
             <div style={{ textAlign: 'center', backgroundColor: 'white', color: '#333' }}>
                 <img
                     src={presentationBanner}
@@ -12,19 +12,19 @@ const Author_Presentation = () => {
                     className=' w-full'
                 />
             </div>
-            <div style={{ padding: '0 10%', textAlign: 'left' }} >
+            <div style={{ padding: '0 10%', textAlign: 'left' }} className='text-center sm:text-left  mr-5 ml-5' >
                 <p className=' font-bold text-4xl text-center my-10 '>Presentation Guide</p>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2'>Tips for a Good Presentation</p>
-                    <p className='text-sm'>Whether you are a seasoned presenter or a nervous first-timer, the following tips are intended as a simple guide.</p>
+                    <p className=' font-bold mb-2'>Tips for a Good Presentation</p>
+                    <p className='text-sm text-justify'>Whether you are a seasoned presenter or a nervous first-timer, the following tips are intended as a simple guide.</p>
                 </div>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2 '>What to Bring</p>
-                    <p className='text-sm'>Bring your presentation on a USB drive, bring a copy on a second USB as a back-up and email the file to yourself ahead of time in case of loss or failure. In addition, you may bring along printed copies of your presentation, your full paper or other handouts, to distribute to audience members. We also recommend that you bring business cards.</p>
+                    <p className=' font-bold mb-2 '>What to Bring</p>
+                    <p className='text-sm text-justify '>Bring your presentation on a USB drive, bring a copy on a second USB as a back-up and email the file to yourself ahead of time in case of loss or failure. In addition, you may bring along printed copies of your presentation, your full paper or other handouts, to distribute to audience members. We also recommend that you bring business cards.</p>
                 </div>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2 '>Checklist</p>
-                    <div className='ml-2'>
+                    <p className=' font-bold mb-2 '>Checklist</p>
+                    <div className='ml-2 text-justify'>
                         <li className='text-sm'>Presentation on a USB drive</li>
                         <li className='text-sm'>Back-up of presentation (USB and email)</li>
                         <li className='text-sm'>Printouts of presentation, full paper and/or relevant handouts (optional)</li>
@@ -32,28 +32,28 @@ const Author_Presentation = () => {
                     </div>
                 </div>
                 <div>
-                    <p>Important: Please ensure that the file name of your presentation begins with your own name or Paper ID, rather than naming the file “ICMSN” or “UK”. This ensures you don’t waste valuable presentation time searching for the file.</p>
+                    <p><span className='font-bold'>Important:</span> Please ensure that the file name of your presentation begins with your own name or Paper ID, rather than naming the file “ICMSN” or “UK”. This ensures you don’t waste valuable presentation time searching for the file.</p>
                 </div>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2 '>What to Wear</p>
-                    <p className='text-sm'>There is no official dress code, but delegates generally wear business casual. However, professional attire is appreciated by fellow professionals, so we suggest erring on the smart side – although a tie is not necessary!</p>
+                    <p className=' font-bold mb-2 '>What to Wear</p>
+                    <p className='text-sm text-justify'>There is no official dress code, but delegates generally wear business casual. However, professional attire is appreciated by fellow professionals, so we suggest erring on the smart side – although a tie is not necessary!</p>
                 </div>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2 '>Timing</p>
-                    <p className='text-sm'>Aim to arrive the conference room at least a few minutes in advance before the session starts. Time is of vital importance in making a presentation, so please be aware of the overall length of your presentation, as well as how the time is divided within your presentation, to ensure that you leave the appropriate amount of time for each point.</p>
+                    <p className=' font-bold mb-2 '>Timing</p>
+                    <p className='text-sm text-justify'>Aim to arrive the conference room at least a few minutes in advance before the session starts. Time is of vital importance in making a presentation, so please be aware of the overall length of your presentation, as well as how the time is divided within your presentation, to ensure that you leave the appropriate amount of time for each point.</p>
                 </div>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2 '>How Long Is the Ideal Presentation?</p>
-                    <p className='text-sm'>You have a 15-minute slot allocated in which to present, but your presentation itself should be no longer than 15 minutes. We would suggest aiming for around 12–13 minutes. This will allow ample time for directly engaging with the audience through discussion and questions.</p>
+                    <p className=' font-bold mb-2 '>How Long Is the Ideal Presentation?</p>
+                    <p className='text-sm text-justify'>You have a 15-minute slot allocated in which to present, but your presentation itself should be no longer than 15 minutes. We would suggest aiming for around 12–13 minutes. This will allow ample time for directly engaging with the audience through discussion and questions.</p>
                 </div>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2 '>Practice Makes Perfect</p>
-                    <p className='text-sm'>Long before the conference, make sure you spend ample time distilling and crafting your ideas into a well-thought-out presentation. Do NOT just read out your paper – a well-written paper does not equal an interesting and effective presentation.
+                    <p className=' font-bold mb-2 '>Practice Makes Perfect</p>
+                    <p className='text-sm text-justify'>Long before the conference, make sure you spend ample time distilling and crafting your ideas into a well-thought-out presentation. Do NOT just read out your paper – a well-written paper does not equal an interesting and effective presentation.
                         Practice delivery, timing and use of visuals a long while ahead of the event with your colleagues and friends. Invite feedback and incorporate their criticisms as you polish your presentation, which should be interesting and informative, well paced and lively. Show your passion for your subject: enthusiasm is infectious!</p>
                 </div>
                 <div className=' mt-4 mb-8'>
-                    <p className=' font-medium mb-2 '>Checklist</p>
-                    <div className='ml-2'>
+                    <p className=' font-bold mb-2 '>Checklist</p>
+                    <div className='ml-2 text-justify'>
                         <li className='text-sm'>CHECK your facts, figures and quotes are accurate</li>
                         <li className='text-sm'>CHECK your presentation for grammatical errors and spelling mistakes. If English is not your first language, please consider asking a native speaker to assist with this.</li>
                         <li className='text-sm'>CHECK your timing, ensuring that your presentation is 15 minutes or less in length. Coming in under the 15-minute mark is better than overrunning.</li>
--- src/component/authorSection/Author_Submission.jsx
+++ src/component/authorSection/Author_Submission.jsx
@@ -12,43 +12,44 @@ const Submission = () => {
             <div style={{ padding: '0 10%', textAlign: 'left' }} >
                 <p className=' font-bold text-4xl text-center my-10 '>Submissions</p>
                 <p className=' text-justify pb-10'>
-                    <div>
-                        <p>Submission Methods:</p>
+                    <div className=' mt-10 mb-5'>
+                        <p className=' text-2xl pb-4 text-green-800 font-semibold'>Submission Methods:</p>
                         <li><a href=''>Online Submission System</a></li>
                         <li>Send via mail: smet2024@nitkkr.ac.in</li>
                     </div>
-                    <div>
-                        <p>Full Paper Submission <a href=''> LINK HERE </a></p>
-                        <div>
-                            <p>Step 1</p>
+                    <div className=' mt-10 mb-5'>
+                        <p className=' text-2xl pb-4 text-green-800 font-semibold'>Full Paper Submission <a href=''>( LINK HERE )</a></p>
+                        <div className='pb-2'>
+                            <p className=' text-green-600 font-medium mb-2'>Step 1:</p>
                             <p>Authors should only submit originally written, unpublished work to SMET 2024. All papers must be written in English and has no less than 6 pages.</p>
                         </div>
-                        <div>
-                            <p>Step 2</p>
+                        <div className='pb-2'>
+                            <p className=' text-green-600 font-medium mb-2'>Step 2:</p>
                             <p>To make sure that the page-size, margin and font-size requirements are in required template, prospective authors are strongly advised to download above the full paper template before submission.</p>
                         </div>
                     </div>
-                    <div>
+                    <div className=' mt-10 mb-5'>
                         <div>
+                        <p className=' text-2xl pb-4 text-green-800 font-semibold'>Abstract Submission</p>
                             <p>
                             If you simply like to share some ideas without publication, we do welcome you submit an one-page abstract. Accepted abstract will be invited to give presentation at the conference, the abstract will NOT be published. Please download the abstract template before submitting your abstract.
                             </p>
                         </div>
                     </div>
-                    <div>
-                        <p>Important Dates</p>
+                    <div className=' mt-10 mb-10'>
+                        <p className=' text-2xl pb-4 text-green-800 font-semibold'>Important Dates</p>
                         <table  style={{  borderCollapse: 'collapse',  backgroundColor: 'white' }}>
                             <tr>
-                                <td style={{ border: '1px solid black', padding: '2px' }}>Submission Deadline</td>
-                                <td style={{ border: '1px solid black', padding: '2px' }}>January 11, 2024</td>
+                                <td style={{ border: '1px solid black', padding: '14px' }}  >Submission Deadline</td>
+                                <td style={{ border: '1px solid black', padding: '14px' }}  >January 11, 2024</td>
                             </tr>
                             <tr>
-                                <td style={{ border: '1px solid black', padding: '2px' }}>Notification Deadline</td>
-                                <td style={{ border: '1px solid black', padding: '2px' }}>January 26, 2024</td>
+                                <td style={{ border: '1px solid black', padding: '14px' }}  >Notification Deadline</td>
+                                <td style={{ border: '1px solid black', padding: '14px' }}  >January 26, 2024</td>
                             </tr>
                             <tr>
-                                <td style={{ border: '1px solid black', padding: '2px' }}>Registration Deadline</td>
-                                <td style={{ border: '1px solid black', padding: '2px' }}>February 2, 2024</td>
+                                <td style={{ border: '1px solid black', padding: '14px' }}  >Registration Deadline</td>
+                                <td style={{ border: '1px solid black', padding: '14px' }}  >February 2, 2024</td>
                             </tr>
                         </table>
                     </div>
